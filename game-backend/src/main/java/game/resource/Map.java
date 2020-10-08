826512e4e7060711e8524316f12c8ea8645a39f5
Paul Berthelot <>
2020-10-08 10:01:16 +0200

    ajout map

---
+++ game-backend/src/main/java/game/resource/Map.java
@@ -0,0 +1,14 @@
+package game.resource;
+
+import io.swagger.annotations.Api;
+
+import javax.inject.Singleton;
+import javax.ws.rs.Path;
+
+@Singleton
+@Path("map")
+@Api(value = "map")
+public class Map {
+
+
+}
