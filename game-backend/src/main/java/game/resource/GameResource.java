f66c13b8dae3d437e9a29a7386b363b5e724145c
Paul Berthelot <>
2020-10-08 10:56:45 +0200

    ajout classes3

--- game-backend/src/main/java/game/resource/GameResource.java
+++ game-backend/src/main/java/game/resource/GameResource.java
@@ -1,13 +1,10 @@
 package game.resource;
 
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import io.swagger.annotations.Api;
 
 import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
@@ -36,6 +33,13 @@ public class GameResource {
             .stream()
                 .
 
+    }
+
+    @POST
+    @Path("api/v1/maps")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response setMaps(){
+        
     }
 
 }
