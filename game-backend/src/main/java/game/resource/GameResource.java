4df0b856a1d96943b28e28c7ae5063a896337a80
Baptiste <baptiste.broyer@insa-rennes.fr>
2020-10-08 10:20:49 +0200

    Firt route

--- game-backend/src/main/java/game/resource/GameResource.java
+++ game-backend/src/main/java/game/resource/GameResource.java
@@ -1,8 +1,12 @@
 package game.resource;
 
+import com.google.common.net.MediaType;
+import com.sun.research.ws.wadl.Response;
 import io.swagger.annotations.Api;
 
 import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Singleton
@@ -13,4 +17,10 @@ public class GameResource {
     public GameResource() {
         super();
     }
+
+    @GET
+    @Path("api/v1/maps")
+    @Consumes({MediaType.APPLICATION_XML_UTF_8})
+    public Response getMaps()
+
 }
