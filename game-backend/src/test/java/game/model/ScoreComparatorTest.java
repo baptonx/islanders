c7caaceec69100b86744667342b9e1fc63cebd32
Baptiste <baptiste.broyer@insa-rennes.fr>
2020-11-02 20:56:39 +0100

    Tests scoreComparator

--- game-backend/src/test/java/game/model/ScoreComparatorTest.java
+++ game-backend/src/test/java/game/model/ScoreComparatorTest.java
@@ -8,16 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class ScoreComparatorTest {
     Score s1;
     Score s2;
+    Score s3;
+    ScoreComparator sc;
 
     @BeforeEach
     void setUp() {
         s1 = new Score("hugz", 54);
         s2 = new Score("bapt", 69);
-        ScoreComparator sc = new ScoreComparator();
+        s3 = new Score("hugz", 69);
+        sc = new ScoreComparator();
     }
 
     @Test
-    void compare() {
-
+    void greaterThan() {
+        assertTrue(sc.compare(s1,s2) <0);
+    }
+    @Test
+    void smallerThan() {
+        assertTrue(sc.compare(s2,s1) >0);
+    }
+    @Test
+    void equals() {
+        assertTrue(sc.compare(s2,s3) == 0);
     }
 }
