153114bb0d14bf23a458d73fcddce764128e7394
Hugo Martineau <hugo.martineau@insa-rennes.fr>
2020-11-02 10:13:51 +0100

    test

--- game-backend/src/test/java/game/model/MapFactoryTest.java
+++ game-backend/src/test/java/game/model/MapFactoryTest.java
@@ -3,6 +3,10 @@ package game.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapFactoryTest {
@@ -14,14 +18,25 @@ class MapFactoryTest {
     }
 
     @Test
-    void testNewMapWithoutConstructor(){
+    void newMapWithoutParameters(){
         MapResource map = mf.newMap();
         assertNotNull(map.getName());
-        assertArrayEquals(map.getTabTiles(), );
+        assertArrayEquals(map.getTabTiles(), new Tile[100]);
     }
 
     @Test
-    void testRandomMap() {
+    void newMapWithName(){
+        MapResource map = mf.newMap("Super map");
+        assertEquals(map.getName(), "Super map");
+        assertArrayEquals(map.getTabTiles(), new Tile[100]);
+    }
 
+    @Test
+    void randomMap() {
+        MapResource map = mf.newRandomMap();
+        assertNotNull(map.getName());
+        Arrays.stream(map.getTabTiles()).forEach(object ->
+                assertThat(object, instanceOf(Tile.class))
+        );
     }
 }
