92da61c5d98d77dfd112de706fd8cf73678d892d
Paul Berthelot <>
2020-11-03 15:39:35 +0100

    commitPaul

---
+++ game-backend/src/main/java/game/model/PutCityBlock.java
@@ -0,0 +1,11 @@
+package game.model;
+
+public class PutCityBlock {
+    private int position;
+    private int typeCityBlock;
+
+    public PutCityBlock(int position, int typeCityBlock) {
+        this.position = position;
+        this.typeCityBlock = typeCityBlock;
+    }
+}
