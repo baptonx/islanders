655a8199ce792b5fe0225a314b122ca4d36c1dba
Hugo Martineau <hugo.martineau@insa-rennes.fr>
2020-11-03 14:50:31 +0100

    test

--- game-backend/src/main/java/game/model/Storage.java
+++ game-backend/src/main/java/game/model/Storage.java
@@ -33,6 +33,10 @@ public class Storage {
         return this.listMap;
     }
 
+    public List<String> getMapName(){
+        return listMap.stream().map(m -> m.getName()).collect(Collectors.toList());
+    }
+
     public int listMapSize() {
         return listMap.size();
     }
