098b46e4918a24a6862273595175d25d22e9fce9
Hugo Martineau <hugo.martineau@insa-rennes.fr>
2020-11-19 11:14:45 +0100

    game component

--- game-frontend/src/app/app-routing.module.ts
+++ game-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MapComponent} from './map/map.component';
+import {GameComponent} from './game/game.component';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
+  { path : 'hugo', component : GameComponent },
   { path: '',
     redirectTo: '/map',
     pathMatch: 'full'
