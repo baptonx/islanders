36f67ae6597efaac43a6533a0ee959636920a500
Baptiste <baptiste.broyer@insa-rennes.fr>
2020-11-21 15:40:05 +0100

    inventoryComponent

--- game-frontend/src/app/inventory/inventory.component.ts
+++ game-frontend/src/app/inventory/inventory.component.ts
@@ -7,9 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
-  availableCityBlock: Array<number> = [0, 0, 0, 0];
-
-  x = 8;
+  availableCityBlock: Array<number> = [12, 1, 2, 8];
 
   constructor() {
   }
@@ -17,4 +15,19 @@ export class InventoryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public getCityBlockSvg(x: number): string {
+    if (x === 0) {
+      return 'assets/house.svg';
+    } else if (x === 1) {
+      return 'assets/fountain.svg';
+    } else if (x === 2) {
+      return 'assets/wind-turbine.svg';
+    } else if (x === 3) {
+      return 'assets/circus.svg';
+    }
+  }
+
+  public getCityBlockRemaining(x: number): number {
+    return this.availableCityBlock[x];
+  }
 }
