9b522719fd4a61c81811cec191ce75caebd1aadb
Paul Berthelot <>
2020-12-03 09:03:44 +0100

    Paul

---
+++ game-frontend/src/app/model/put-city-block-undoable.spec.ts
@@ -0,0 +1,7 @@
+import { PutCityBlockUndoable } from './put-city-block-undoable';
+
+describe('PutCityBlockUndoable', () => {
+  it('should create an instance', () => {
+    expect(new PutCityBlockUndoable()).toBeTruthy();
+  });
+});
