561d78ef30c1783bcb410f7cdf38432c4b180eb5
Paul Berthelot <>
2021-01-07 12:27:20 +0100

    commitPaul Command Rename Marche pas

--- game-frontend/src/app/model/command-rename.ts
+++ game-frontend/src/app/model/command-rename.ts
@@ -3,36 +3,12 @@ import {InfogameService} from '../service/infogame.service';
 import {Command} from './command';
 
 export class CommandRename extends Command implements Undoable {
-  type = 'game.Model.MoveCityBlock';
+  type = 'game.model.CommandRename';
+  nouveauNomJoueur: string;
   public mementoNomJoueur: string;
 
-  public constructor(private infoGameService: InfogameService, private nouveauNomJoueur: string) {
+  public constructor(nouveauNomJoueur: string) {
     super();
-    this.createMemento();
-    this.execution();
-  }
-
-
-  // Create the memento for undoing the command
-  protected createMemento() {
-    // We copy the current state of the MapService
-    this.mementoNomJoueur = this.infoGameService.nomJoueur;
-  }
-
-
-  protected execution(): void {
-    this.redo();
-  }
-
-  undo(): void {
-    this.infoGameService.nomJoueur = this.mementoNomJoueur;
-  }
-
-  redo(): void {
-    this.infoGameService.nomJoueur = this.nouveauNomJoueur;
-  }
-
-  getUndoName(): string {
-    return 'Undo name';
+    this.nouveauNomJoueur = nouveauNomJoueur;
   }
 }
